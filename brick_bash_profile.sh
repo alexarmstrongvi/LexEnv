64527340bf39401464778c97d76823904acc5c3c
Alex Armstrong <armstro1@uci.edu>
2018-12-10 06:58:13 -0800

    Changed root version in brick profile setup

--- brick_bash_profile.sh
+++ brick_bash_profile.sh
@@ -11,7 +11,7 @@ export PYTHONPATH
 ################################################################################
 # Environment Configuration
 export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
-export ALRB_rootVersion=6.14.04-x86_64-slc6-gcc62-opt
+export ALRB_rootVersion=6.12.06-x86_64-slc6-gcc62-opt
 export USER='alarmstr'
 
 # Dont write .pyc files

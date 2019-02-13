7a553119da71c64ac1c8253f21db3b42a1a7c250
Alex Armstrong <armstro1@uci.edu>
2019-02-13 04:07:13 -0800

    added atlas sw aliases

--- brick_bash_profile.sh
+++ brick_bash_profile.sh
@@ -23,7 +23,12 @@ alias setupATLAS='source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh'
 alias vomsAtlas='voms-proxy-init -voms atlas -valid 96:00'
 alias kcern='kinit -f $USER@CERN.CH'
 alias condor_ls='condor_q -nobatch | head; echo ...; condor_q -nobatch | grep -v 0+00:00:00 | tail; printf "Started but idle jobs : "; condor_q -nobatch | grep -v 0+00:00:00 | grep " I " | wc -l;'
-
+alias jodir='cd /cvmfs/atlas.cern.ch/repo/sw/Generators/MC15JobOptions/'
+function atlas_sw () {
+    full_ver=$1
+    short_ver=$(echo $full_ver | grep -o ^[0-9]*.[0-9]*)
+    echo "/cvmfs/atlas.cern.ch/repo/sw/software/${short_ver}/AnalysisBase/${full_ver}/InstallArea/*"
+}
 # Includes (must come after defining PATH variables)
 source ~/LexTools/bashTools.sh
 remove_PATH_duplicates

a0ac6b13a4009ac84ba3a4de52e7d5fbeb1d87f9
Alexander Armstrong <aarmstrong@arete.com>
2023-01-30 12:24:10 -0500

    Merged duplicate print_shell_type.sh

--- print_shell_type.sh
+++
@@ -1,2 +0,0 @@
-source ~/LexEnv/bash_utils.sh
-print_shell_type
--- scripts/print_shell_type.sh
+++ scripts/print_shell_type.sh
@@ -1,66 +1,24 @@
-#!/bin/bash
+#!/usr/bin/env bash
+source ~/LexEnv/bash_utils.sh
+print_shell_type
 
 # To demo all types of shells, open a terminal emulator and follow steps below 
 
 # The current interactive shell type
 # >> source print_shell_type.sh
 
-# Non-interactive non-login shell (.bashrc executed)
+# Non-interactive non-login shell ($BASH_ENV sourced)
 # >> ./print_shell_type.sh
 
-# Non-interactive login shell
-# >> bash -l print_shell_type
+# Non-interactive login shell ($BASH_ENV sourced)
+# >> bash -l print_shell_type.sh
 
-# Interactive non-login shell (.bashrc executed)
+# Interactive non-login shell (.bashrc sourced)
 # >> bash
 # >> source print_shell_type.sh
 # >> exit
 
-# Interactive login shell (.bash_profile executed)
+# Interactive login shell (.bash_profile sourced)
 # >> bash -l
 # >> source print_shell_type.sh
 # >> exit
-
-function is_interative_shell() {
-    [[ $- == *i* ]]
-}
-
-function is_zshell() {
-    [ -n "$ZSH_VERSION" ]
-}
-
-function is_bash_shell() {
-    [ -n "$BASH_VERSION" ]
-}
-
-function is_login_shell() {
-    { is_zshell && [[ -o login ]]; } || { is_bash_shell && shopt -q login_shell; }
-}
-
-
-function print_shell_type() {
-    printf "Running in "
-    if is_interative_shell; then
-        printf "an interactive "
-    else
-        printf "a non-interactive "
-    fi
-
-    #if 
-    if is_login_shell; then
-        printf "login "
-    else
-        printf "non-login "
-    fi
-
-    if is_zshell; then
-        printf "Z shell"
-    elif is_bash_shell; then
-        printf "bash shell"
-    else
-        printf "unknown shell"
-    fi
-    printf "\n"
-}
-
-print_shell_type

42fa56b66c23b5db0d6af2ee5d9f007f8209a0f5
Alex Armstrong <alex.armstrong.vi@gmail.com>
2023-03-09 23:14:29 -0800

    Added inital VS Code configuration files

---
+++ vscode/README.md
@@ -0,0 +1,14 @@
+VS Code configuration
+================================================================================
+
+# Installing extensions
+```bash
+source ./install_extensions.sh
+```
+
+# Adding configuration files
+1. Locate location of VS Code User configuration files
+    * MacOS: $HOME/Library/Application Support/Code/ 
+1. Replace relevant settings files with symlinks
+
+Note that VS Code allows `//` for comments in JSON files
---
+++ vscode/install_extensions.sh
@@ -0,0 +1,28 @@
+################################################################################
+# Script to install VS Code extensions
+#
+# Get started with code 
+# >> code --list-extensions > install_extensions.sh
+# Note that some packages install their own extensions
+################################################################################
+# Text editing
+code --install-extension stkb.rewrap
+
+# Python
+code --install-extension ms-python.python
+code --install-extension ms-python.isort
+
+# Jupyter notebooks
+code --install-extension ms-toolsai.jupyter
+
+# Git
+code --install-extension eamodio.gitlens
+code --install-extension mhutchie.git-graph
+
+# C++ & CMake
+code --install-extension ms-vscode.cpptools-extension-pack
+
+# Vim mode
+code --install-extension vscodevim.vim
+
+
